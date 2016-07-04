5255e18306b0a9e4e9654bf723540f89e9dd0074
gerardolsj <lsjgerardo@gmail.com>
2016-07-04 17:47:23 -0500

    mulyiple components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
-export class Hero {
-  id: number;
-  name: string;
-}
+
 
 
 
@@ -17,14 +16,8 @@ export class Hero {
         <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
     </ul>
- <div *ngIf="selectedHero">   
-    <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-    <label>name: </label>
-    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-</div>
-</div>
+<my-hero-detail [hero]="selectedHero"></my-hero-detail>
+
 
 
     `,
@@ -76,7 +69,8 @@ export class Hero {
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+directives : [HeroDetailComponent]
 
 
 })
