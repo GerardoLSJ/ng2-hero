62108fc6de66a8199a89ee9129299a5c9f37e956
gerardolsj <lsjgerardo@gmail.com>
2016-07-05 12:29:00 -0500

    asdf

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,27 +1,40 @@
 import {Component, Input} from '@angular/core'
 import {Hero} from './hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from './hero.service';
+import { OnInit, OnDestroy } from '@angular/core';
 
 
 @Component({
-    selector: 'my-hero-detail',
-    template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-  </div>
-`
+  selector: 'my-hero-detail',
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css']
+  
+})
+export class HeroDetailComponent implements OnInit, OnDestroy {
+  hero: Hero[];
+  sub: any;
 
+  constructor(
+  private heroService: HeroService,
+  private route: ActivatedRoute) {
+}
 
-}) 
+    ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+    });
+  }
 
+  ngOnDestroy() {
+  this.sub.unsubscribe();
+}
+  goBack() {
+  window.history.back();
+}
 
-export class HeroDetailComponent { 
-  @Input()
-  hero: Hero;
 
 }
 
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,4 +6,9 @@ export class HeroService{
     getHeroes(){
        return Promise.resolve(HEROES);
     }
+    getHero(id: number) {
+  return this.getHeroes()
+             .then(heroes => heroes.filter(hero => hero.id === id)[0]);
+}
+
 }
